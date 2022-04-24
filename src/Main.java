27d3740b2197c139b7aa5d1cace070f3f8ea7ca0
jgrzesik <jozef.grzesik239@gmail.com>
2022-04-24 23:27:19 +0200

    dodano podstawowe zliczanie znaków

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,8 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         input = scanner.nextLine();
 
-        System.out.println(countWordsUsingSplit(input));
+        System.out.println("words: " + countWordsUsingSplit(input));
+        System.out.println("characters: " + countCharacters(input));
     }
     public static int countWordsUsingSplit(String input) {
         if (input == null || input.isEmpty()) {
@@ -17,6 +18,9 @@ public class Main {
         }
         String[] words = input.split("\\s+");
         return words.length;
+    }
+    public static int countCharacters(String input){
+        return input.length();
     }
 
 }
