1e56422c47bddf865aae934d98714bdd03050eec
jgrzesik <jozef.grzesik239@gmail.com>
2022-04-25 00:40:50 +0200

    dodano metodę zliczającą ilość wystapień podanego znaku

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
         String input = "Imagine living in a country with no basic human rights, lack of food, no freedom of speech and even no freedom of thought.";
         System.out.println(input);
+        char example = 'i';
 //        System.out.print("Enter text: ");
 //        Scanner scanner = new Scanner(System.in);
 //        input = scanner.nextLine();
@@ -13,6 +14,7 @@ public class Main {
         System.out.println("words: " + countWordsUsingSplit(input));
         System.out.println("characters: " + countCharacters(input));
         System.out.println("characters without spaces: " + countCharactersWithoutSpaces(input));
+        System.out.printf("number of %c is %d%n",example,countGivenCharacter(input, example));
     }
     public static int countWordsUsingSplit(String input) {
         if (input == null || input.isEmpty()) {
@@ -27,5 +29,16 @@ public class Main {
     public static int countCharactersWithoutSpaces(String input){
         return input.replace(" ", "").length();
     }
+    public static int countGivenCharacter(String input, char chr){
+        int totalChars = 0;
+        char tmp;
 
+        for (int i = 0; i < input.length(); i++) {
+            tmp = input.charAt(i);
+            if (tmp == chr){
+                totalChars++;
+            }
+        }
+        return totalChars;
+    }
 }
